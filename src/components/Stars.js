22168ab0ca087a03a8c281c9d6d30688ef15894d
Meghan Ryan Morris <megrymo@gmail.com>
2022-08-06 17:41:55 -0500

    clean up, comments

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,5 +1,15 @@
 import React from 'react';
 const Stars = () => {
+  /*
+    if given more time, i would have liked to explore the following approaches:
+      1. try a few css mask animation options, although the browser support could be
+        better for this.
+      2. a bit of a hack maybe, but create a video w/ the twinkling animation for
+        desktop + a gif fallback for mobile.
+      3. go all out exporting + positioning each star and testing whether so many
+        animations on the page would severely impact performance. i believe it would
+        work okay but warrants testing.
+  */
   return (
     <div className="background__star-container">
       {Array.from(Array(7), (e, i) =>
@@ -15,7 +25,7 @@ const Stars = () => {
           )
         }
       )}
-     <img className="background__stars"
+      <img className="background__stars"
         src='./images/stars/static-stars.png'
         srcSet='./images/mobile/stars.png 750w ./images/stars/static-stars.png 1280w'
         alt="peach-colored cloud"
